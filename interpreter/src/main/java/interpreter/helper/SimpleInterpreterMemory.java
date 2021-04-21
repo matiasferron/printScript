2aaf5220d97731ca6ca1f15857ac870f49979bd7
matias <matiferron98@gmail.com>
2021-04-21 20:53:32 -0300

    interpreter

--- interpreter/src/main/java/interpreter/helper/SimpleInterpreterMemory.java
+++ interpreter/src/main/java/interpreter/helper/SimpleInterpreterMemory.java
@@ -12,25 +12,18 @@ import java.util.Map;
 
 import static token.TokenType.LET;
 
-public class InterpreterMemory implements InterpreterHelper {
-
+public class SimpleInterpreterMemory implements InterpreterHelper {
     private final Map<String, VariableDefinitionDataImplementation> values;
     private final List<String> printedValues;
-    private boolean isTemporal;
-    private Map<String, VariableDefinitionDataImplementation> temporalValues;
 
 
-    public InterpreterMemory() {
+    public SimpleInterpreterMemory() {
         this.values = new HashMap<>();
         this.printedValues = new ArrayList<>();
-        this.isTemporal = false;
-        this.temporalValues = new HashMap<>();
+
     }
 
     public Object get(Token name) {
-        if (isTemporal)
-            return getTempValues(name);
-
 
         if (values.containsKey(name.getTokenValue()))
             return values.get(name.getTokenValue()).getValue();
@@ -40,12 +33,9 @@ public class InterpreterMemory implements InterpreterHelper {
     }
 
     public void turnOnTemporalSpace(){
-        this.isTemporal = true;
     }
 
     public void turnOffTemporalSpace(){
-        this.isTemporal = false;
-        this.temporalValues = new HashMap<>();
     }
 
     public List<String> getPrintedValues() {
@@ -57,23 +47,10 @@ public class InterpreterMemory implements InterpreterHelper {
     }
 
     public void addVariableDefinition(String varName, TokenType keyword, TokenType type, Object value, Token name) {
-        if (isTemporal) {
-            if (values.containsKey(varName)){
-                throw new InterpretException(name, "Variable " + varName + " already defined. Error");
-            }
-            temporalValues.put(varName, new VariableDefinitionDataImplementation(keyword, type, value));
-            return;
-        }
-
         values.put(varName, new VariableDefinitionDataImplementation(keyword, type, value));
     }
 
     public void setVariableValue(Token varName, Object value) {
-        if (isTemporal) {
-            setTemporalVariableValue(varName, value);
-            return;
-        }
-
         if (values.containsKey(varName.getTokenValue())) {
             checkIfContainAndUpdate(varName, value);
             return;
@@ -85,17 +62,6 @@ public class InterpreterMemory implements InterpreterHelper {
 
     // Private methods
 
-    private Object getTempValues(Token name) {
-        if (values.containsKey(name.getTokenValue())) {
-            return values.get(name.getTokenValue()).getValue();
-        }
-
-        if (temporalValues.containsKey(name.getTokenValue())) {
-            return temporalValues.get(name.getTokenValue()).getValue();
-        }
-
-        throw new InterpretException(name, "Variable " + name.getTokenValue() + " not found");
-    }
 
     private void checkIfContainAndUpdate(Token varName, Object value) {
 
@@ -133,32 +99,11 @@ public class InterpreterMemory implements InterpreterHelper {
         }
     }
 
-    private void setTemporalVariableValue(Token varName, Object value) {
-        if (values.containsKey(varName.getTokenValue())) {
-            checkIfContainAndUpdate(varName, value);
-            return;
-        }
-
-        if (temporalValues.containsKey(varName.getTokenValue())) {
-            checkIfContainTemporalValueAndUpdate(varName, value);
-            return;
-        }
-
-        throw new InterpretException(varName, "Undefined variable '" + varName.getTokenValue() + "'.");
-    }
-
-    private void checkIfContainTemporalValueAndUpdate(Token varName, Object value) {
-
-        VariableDefinitionDataImplementation tempValue = temporalValues.get(varName.getTokenValue());
-
-        updateValue(tempValue, varName, value, temporalValues);
-    }
-
-    private void updateValue(VariableDefinitionDataImplementation oldValue, Token varName, Object value, Map<String, VariableDefinitionDataImplementation> mapValues) {
+    private void updateValue(VariableDefinitionDataImplementation oldValue, Token varName, Object value, Map<String, VariableDefinitionDataImplementation> values) {
         if (oldValue.getVarDefinitionKey() == LET) {
             checkType(oldValue.getType(), value, varName);
             oldValue.setValue(value);
-            mapValues.put(varName.getTokenValue(), oldValue);
+            values.put(varName.getTokenValue(), oldValue);
         } else {
             throw new InterpretException(varName, "Constant cannot be changed");
         }
